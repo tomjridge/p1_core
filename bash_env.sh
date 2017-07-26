68db19476b660f47e4cda84ccda05353405492a6
Tom Ridge <tom.j.ridge@googlemail.com>
2017-07-26 15:51:32 +0100

    +

---
+++ bash_env.sh
@@ -0,0 +1,9 @@
+set -a # export all vars
+#set -x # debug
+
+libname="p1_core"
+required_packages="tjr_lib" #,astring ,ppx_string_interpolate
+description="Core p1 parsing library"
+
+source bash_env.common
+
